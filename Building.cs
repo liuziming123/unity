9fecbd986a10914f2fcc35a606288710bda76a13
liuziming123 <572782960@qq.com>
2016-07-13 16:36:09 +0800

    修改了速度

---
+++ Building.cs
@@ -0,0 +1,25 @@
+
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Building : MonoBehaviour {
+	
+	[HideInInspector]
+	public bool buildingActive;
+	[HideInInspector]
+	public int buildIndex;
+	
+	public static Building instance;
+	
+	void Start(){
+		instance = this;
+	}
+	
+	//Reset building when character die	
+	public void Reset(){
+		buildingActive = false;
+		this.transform.parent = null;
+	}
+}
